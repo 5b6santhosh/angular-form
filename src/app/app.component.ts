7108692858e46598ba71609c34e4c309c0c1c4f5
5b6santhosh <5b6santhosh@gmail.com>
2019-07-25 18:33:01 +0530

    forms created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,12 @@ export class AppComponent implements OnInit {
 
   }
   form= new FormGroup({
-  FirstName: new FormControl('', [Validators.required])
+  FirstName: new FormControl('', [Validators.required]),
+  LastName: new FormControl('', [Validators.required]),
+  Email: new FormControl('', [Validators.required]),
+  MobileNumber:new FormControl('',[Validators.required]),
+  LandLineNumber:new FormControl('',[Validators.required]),
+
   });
   sub(v){
      if (this.form.invalid) {
